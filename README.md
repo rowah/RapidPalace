3eb0cdc0f10a07aa3d8f8c5f3971d261f4c0ae39
Rowa <jrowah@gmail.com>
2022-05-25 20:57:12 +0300

    Creates a file and writes hello world

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nodejscrash
+# nodejscrashCourse
--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,9 +2,17 @@ const fs = require("fs");
 const path = require("path");
 
 //creating a folder
-fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
-  if (err) throw err;
-  console.log("Folder created...");
-}); // when we run node fs_demo.js we see a folder called test created
+// fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+//   if (err) throw err;
+//   console.log("Folder created...");
+// }); // when we run node fs_demo.js we see a folder called test created
 
 //create and write to file
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Hello World",
+  (err) => {
+    if (err) throw err;
+    console.log("File written to...");
+  } //creates file hello.txt on the text folder
+);
