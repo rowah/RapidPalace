af11f94b40906829b4b56fa93694d228664dbc68
Rowa <jrowah@gmail.com>
2022-05-25 22:00:24 +0300

    gets urls and host name

---
+++ reference/url_demo.js
@@ -0,0 +1,10 @@
+const url = require("url");
+
+const myUrl = new URL("https://jrowah.com/");
+console.log(myUrl.href);
+
+//Host (root domain)
+console.log(myUrl.host);
+
+//HostaNAME (DOESNT GET THE PORT)
+console.log(myUrl.hostname);
