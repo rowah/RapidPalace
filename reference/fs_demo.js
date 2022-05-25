ce2f2fc0f7a40498b5b7e5e5e7f870dda43ff40c
Rowa <jrowah@gmail.com>
2022-05-25 21:18:35 +0300

    Renames a file

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,28 +2,28 @@ const fs = require("fs");
 const path = require("path");
 
 //creating a folder
-// fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
-//   if (err) throw err;
-//   console.log("Folder created...");
-// }); // when we run node fs_demo.js we see a folder called test created
+fs.mkdir(path.join(__dirname, "/test"), {}, (err) => {
+  if (err) throw err;
+  console.log("Folder created...");
+}); // when we run node fs_demo.js we see a folder called test created
 
-// //create and write to file
-// fs.writeFile(
-//   path.join(__dirname, "/test", "hello.txt"),
-//   "Hello World",
-//   (err) => {
-//     if (err) throw err;
-//     console.log("File written to...");
-//     fs.appendFile(
-//       path.join(__dirname, "/test", "hello.txt"),
-//       " This is another added text",
-//       (err) => {
-//         if (err) throw err;
-//         console.log("File written to...");
-//       } //creates file hello.txt on the text folder
-//     );
-//   } //creates file hello.txt on the text folder
-// );
+//create and write to file
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Hello World",
+  (err) => {
+    if (err) throw err;
+    console.log("File written to...");
+    fs.appendFile(
+      path.join(__dirname, "/test", "hello.txt"),
+      " This is another added text",
+      (err) => {
+        if (err) throw err;
+        console.log("File written to...");
+      } //creates file hello.txt on the text folder
+    );
+  } //creates file hello.txt on the text folder
+);
 //adding text to the hello.txt use appendFile as writeFile overwrites what was already in the file
 
 //Reading a file
@@ -31,3 +31,5 @@ fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
   if (err) throw err;
   console.log(data);
 }); //Reads what is in the file!
+
+//Rename a file
